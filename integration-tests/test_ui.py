e4950cdc1457784527a4ba542b9ae19f9628f160
Raniere Gaia Costa da Silva <Raniere.CostadaSilva@gesis.org>
2025-08-08 14:37:56 +0200

    Change wait strategy

--- integration-tests/test_ui.py
+++ integration-tests/test_ui.py
@@ -44,11 +44,12 @@ async def local_hub_local_binder(request):
             resp = await async_requests.get(url)
             logger.debug("Debugging 1998. Status code is %s", resp.status_code)
             if resp.status_code == 200:
+                yield url
                 break
         except requests.exceptions.ConnectionError:
             pass
-        time.sleep(1)
-    yield url
+        logger.debug("Debugging 1998. Sleeping for %s seconds", resp.status_code)
+        time.sleep(i)
 
     proc.terminate()
     proc.wait()
